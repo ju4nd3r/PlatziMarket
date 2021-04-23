fef955877f68ad8be554e4011c093bf2057c0b14
Juan David Vasquez Rojas - Ceiba Software <juan.vasquez@ceiba.com.co>
2021-04-23 13:15:07 -0500

    Se implemetan los métodos del repositorio con Query methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
 
@@ -12,4 +13,14 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria( int idCategoria ){ return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria); }
+
+    public Optional<Producto> getByCodigoBarras( String codigoBarras ){ return productoCrudRepository.findByCodigoBarras(codigoBarras);}
+
+    public Optional<List<Producto>> getEscasos( int cantidad ){
+        return productoCrudRepository.finByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<List<Producto>> getByNombre( String nombre ){ return productoCrudRepository.findByNombreLike( nombre ); }
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,11 +1,32 @@
 package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Producto;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 /*
 Extendemos de la interface CrudRepository en donde se le indica primero, el tipo
 de Objeto de la entidad y segundo, el tipo de dato de la clave primaria de esa entidad
  */
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    /*
+    Si se fuera a usar de manera nativa, especificando la consulta, lo haríamos con
+    la sgte anotación arriba del método:
+
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+
+    NOTA: Haciendo uso de esta consulta nativa, no es necesario seguir las reglas de nombramiento
+    del los query methods
+     */
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> finByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado );
+
+    Optional<Producto> findByCodigoBarras(String codigoBarras);
+
+    Optional<List<Producto>> findByNombreLike(String nombre);
 }
