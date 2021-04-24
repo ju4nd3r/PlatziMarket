04435b001da3a0d5c6ecb1f7623f0f749320cd3d
Juan David Vasquez Rojas - Ceiba Software <juan.vasquez@ceiba.com.co>
2021-04-24 18:14:30 -0500

    Se implementan métodos faltantes en el ProductoRepository y se decora con @Repository para indicar que dicha clase interactúa directamente con la BD

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,18 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/*
+Como esta es una clase que está interactuando directamente con la base de datos
+es buena práctica decorarla con una anotación, en este caso @Repository.
+
+Con esto le indicamos a Spring que esta clase se encarga de interactuar con la BD
+ */
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -23,4 +31,16 @@ public class ProductoRepository {
     }
 
     public Optional<List<Producto>> getByNombre( String nombre ){ return productoCrudRepository.findByNombreLike( nombre ); }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
 }
